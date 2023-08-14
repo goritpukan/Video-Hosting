3a44bc7347afc6b32d07f7b1d9f149261735cbf3
Vadym <test@gmail.com>
2023-08-14 17:33:21 +0300

    add subscribe and unsubscribe api controllers and routes

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,18 +1,97 @@
 import { createError } from "../error.js"
 import User from "../models/User.js"
 
-export const update = async (req, res, next) => {
+export const updateUser = async (req, res, next) => {
   if (req.params.id === req.user.id) {
     try {
       const updatedUser = await User.findByIdAndUpdate(req.params.id, {
         $set: req.body
       }, { new: true });
-      
-      res.status(200).json(updatedUser);
+
+      const { password, ..._updatedUser } = updatedUser._doc;
+      res.status(200).json(_updatedUser);
+
     } catch (err) {
       next(err);
     }
   } else {
     return next(createError(403, "You can update only your account!"));
+  }
+}
+
+export const deleteUser = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      await User.findByIdAndDelete(req.params.id,);
+      res.status(200).json("User has been deleted");
+
+    } catch (err) {
+      next(err);
+    }
+
+  } else {
+    return next(createError(403, "You can delete only your account!"));
+  }
+}
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    const { password, ..._user } = user._doc;
+    res.status(200).json(_user);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export const subscribe = async (req, res, next) => {
+  try {
+    if (req.params.id === req.user.id) return next(createError(403, "You can't subcsribe yourself"));
+
+    const user = await User.findById(req.user.id);
+    for (let subscribedUsers of user.subscribedUsers) {
+      if (subscribedUsers === req.params.id) return next(createError(403, "You already subscribed"))
+    }
+
+    await User.findByIdAndUpdate(req.user.id, {
+      $push: { subscribedUsers: req.params.id },
+    });
+
+    await User.findByIdAndUpdate(req.params.id, {
+      $inc: { subscribers: 1 },
+    });
+
+    res.status(200).json("Subscription successfull");
+
+  } catch (err) {
+    next(err);
+  }
+}
+
+export const unsubscribe = async (req, res, next) => {
+  try {
+    if (req.params.id === req.user.id) return next(createError(403, "You can't unsubcsribe yourself"));
+
+    const user = await User.findById(req.user.id);
+
+    if(!user.subscribedUsers.length) return next(createError(403, "You are not subscribed"));
+
+    for (let subscribedUsers of user.subscribedUsers) {
+      if (subscribedUsers !== req.params.id) return next(createError(403, "You are not subscribed"));
+    }
+
+    await User.findByIdAndUpdate(req.user.id, {
+      $pull: { subscribedUsers: req.params.id },
+    });
+
+    await User.findByIdAndUpdate(req.params.id, {
+      $inc: { subscribers: -1 },
+    });
+
+    res.status(200).json("Unsubscription successfull");
+
+  } catch (err) {
+    next(err);
   }
 }
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,18 @@
 import express from "express";
 
-import { update } from "../controllers/user.js";
+import { updateUser, deleteUser, getUser, subscribe, unsubscribe } from "../controllers/user.js";
 import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
 
-router.put("/:id", verifyToken, update);
+router.put("/:id", verifyToken, updateUser);
 
+router.delete("/:id", verifyToken, deleteUser);
+
+router.get("/:id", getUser);
+
+router.put("/subscribe/:id", verifyToken, subscribe);
+
+router.put("/unsubscribe/:id", verifyToken, unsubscribe);
 
 export default router;
